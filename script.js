be5081715ae5c2220e96285c18fa30231a3f57ad
Delete606 <157417075+Delete606@users.noreply.github.com>
2024-01-30 09:50:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayTime() {
+    let element = document.getElementById("clock"); // Найти элемент с id="clock" 
+    let now = new Date();    // Получить текущее время
+    element.innerHTML = now.toLocaleTimeString(); // Отобразить время
+    setTimeout(displayTime, 1000);    // Вызывать функцию каждую секунду
+}
+window.onload = displayTime; // Начать отображение времени после загрузки документа
+function show ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display = "block";
+}
+
+
